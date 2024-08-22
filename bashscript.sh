d2aa71ccb1f186ab14636006c099077656b6cd0e
Rodgers Munene <90693213+RodgersMunene@users.noreply.github.com>
2024-08-23 01:43:33 +0300

    Add files via upload

---
+++ bashscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+
+then
+echo "You Forgot and IP Address"
+echo "Syntax: ./ipsweep.sh 192.168.0"
+
+else
+for ip in `seq 1 254`; do
+ping -c 1 $1.$ip | grep "64 bytes" |cut -d " " -f 4 |tr -d ":" &
+done
+fi
